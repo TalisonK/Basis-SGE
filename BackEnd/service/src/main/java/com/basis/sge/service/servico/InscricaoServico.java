d2e31f61f21fdb2e184b8192f7b32f1368b49f3e
talison <talisonkennedy1@gmail.com>
2021-01-14 17:25:23 -0300

    Inicio do RRS

---
+++ BackEnd/service/src/main/java/com/basis/sge/service/servico/InscricaoServico.java
@@ -0,0 +1,14 @@
+package com.basis.sge.service.servico;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class InscricaoServico {
+
+    public List<Pre>
+}
---
+++ BackEnd/service/src/main/java/com/basis/sge/service/servico/dto/PreInscricaoDTO.java
@@ -0,0 +1,9 @@
+package com.basis.sge.service.servico.dto;
+
+public class PreInscricaoDTO {
+
+    private Integer id;
+    private Integer usuario;
+    private Integer evento;
+    private Integer situacao;
+}
