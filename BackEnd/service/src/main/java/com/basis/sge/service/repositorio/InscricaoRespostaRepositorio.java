1540cb55e2ec58e53984fdd226a6a1908a883bde
Thayane Nunes <thayanenns@gmail.com>
2021-01-15 16:39:26 -0300

    Criação da interface InscricaoRespostaRepositorio -

--- BackEnd/service/src/main/java/com/basis/sge/service/repositorio/InscricaoRespostaRepositorio.java
+++ BackEnd/service/src/main/java/com/basis/sge/service/repositorio/InscricaoRespostaRepositorio.java
@@ -3,6 +3,8 @@ package com.basis.sge.service.repositorio;
 import com.basis.sge.service.dominio.InscricaoResposta;
 import com.basis.sge.service.dominio.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class InscricaoRespostaRepositorio extends JpaRepository<InscricaoResposta, Integer> {
 }
