264c9ed8a95eaff7a3c636d080c5ae8cc93a13c1
Thayane Nunes <thayanenns@gmail.com>
2021-01-14 22:50:04 -0300

    Adiçãodas classes de serviço, recurso, mapper, repository, DTO do dominioUsuario

--- BackEnd/service/src/main/java/com/basis/sge/service/ServiceApplication.java
+++ BackEnd/service/src/main/java/com/basis/sge/service/ServiceApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan(basePackages = {"com.basis.sge.service.servico.mapper"})
+@SpringBootApplication  @ComponentScan(basePackages = {"com.basis.sge.service.servico.mapper"})
 public class ServiceApplication {
 
     public static void main(String[] args) {
--- BackEnd/service/src/main/java/com/basis/sge/service/recurso/UsuarioRecurso.java
+++ BackEnd/service/src/main/java/com/basis/sge/service/recurso/UsuarioRecurso.java
@@ -1,15 +1,50 @@
 package com.basis.sge.service.recurso;
 
+import com.basis.sge.service.servico.dto.UsuarioDTO;
+import com.basis.sge.service.servico.UsuarioServico;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/usuario")
 @RequiredArgsConstructor
 public class UsuarioRecurso {
+    private final UsuarioServico usuarioServico;
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioDTO>> listar(){
+        List<UsuarioDTO> lista = usuarioServico.listar();
+        return ResponseEntity.ok(lista);
+    }
+    @GetMapping({"/id"})
+    public ResponseEntity<UsuarioDTO>obterPorId(@PathVariable Integer id){
+        return ResponseEntity.ok(usuarioServico.obterPorId(id));
+    }
+    @PostMapping
+    public ResponseEntity<UsuarioDTO> criar(@RequestBody UsuarioDTO usuarioDTO){
+        UsuarioDTO usuarioGerado = (usuarioServico.criar(usuarioDTO));
+        return ResponseEntity.ok(usuarioGerado);
+    }
+    @PutMapping
+    public ResponseEntity<UsuarioDTO> atualizar(@RequestBody UsuarioDTO usuarioDTO){
+        UsuarioDTO usuarioAtualizado = usuarioServico.atualizar(usuarioDTO);
+        return ResponseEntity.ok(usuarioAtualizado);
+    }
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Integer id){
+        usuarioServico.deletar(id);
+    }
+
 
 
 }
