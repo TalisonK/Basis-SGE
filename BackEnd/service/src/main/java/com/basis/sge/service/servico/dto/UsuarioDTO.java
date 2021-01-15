4f5504598806b03896b7e8043a5dc4b590ee029a
Thayane Nunes <thayanenns@gmail.com>
2021-01-15 15:18:56 -0300

    Adição de plugins, ajustes em geral

--- BackEnd/service/src/main/java/com/basis/sge/service/servico/dto/UsuarioDTO.java
+++ BackEnd/service/src/main/java/com/basis/sge/service/servico/dto/UsuarioDTO.java
@@ -1,7 +1,12 @@
 package com.basis.sge.service.servico.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 
+@Getter
+@Setter
 public class UsuarioDTO {
     private Integer id;
     private String nome;
