68697b7289985003758de1d0f933b9ff14528051
kayc-drummond-oda <kayc.dr@hotmail.com>
2021-01-20 17:38:56 -0300

    adicionados pacotes builder, recurso e util para iniciar código

---
+++ BackEnd/service/src/test/java/com/basis/sge/service/util/IntTestComum.java
@@ -0,0 +1,39 @@
+package com.basis.sge.service.util;
+
+import com.basis.srs.SistemaDeReservasDeSalasApplication;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.persistence.EntityManager;
+
+@SpringBootTest(classes = SistemaDeReservasDeSalasApplication.class)
+@ExtendWith(SpringExtension.class)
+public abstract class IntTestComum {
+
+    @Autowired
+    private EntityManager em;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    protected MockMvc getMockMvc() {
+        return mockMvc;
+    }
+
+    protected EntityManager getEm() {
+        return em;
+    }
+
+    @BeforeEach
+    void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+}
