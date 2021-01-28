42b4732a32cfdfdd47b0eb6a70ab27cfdf970dff
kayc-drummond-oda <kayc-drummond-oda>
2021-01-28 14:09:02 -0300

    Implementacao de formulario e listagem para pergunta

--- frontend/src/app/modulos/pergunta/pergunta.module.ts
+++ frontend/src/app/modulos/pergunta/pergunta.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PerguntaRoutingModule } from './pergunta-routing.module';
+import { FormularioComponent } from './componentes/formulario/formulario.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [FormularioComponent],
   imports: [
     CommonModule,
     PerguntaRoutingModule
