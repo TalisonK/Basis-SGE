bb87d19ede04710089fafd5b5006f99a64a1e698
kayc-drummond-oda <kayc-drummond-oda>
2021-01-27 23:03:39 -0300

    Configuraçao inicial de Modulo para Pergunta

---
+++ frontend/src/app/modulos/pergunta/pergunta.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PerguntaRoutingModule } from './pergunta-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PerguntaRoutingModule
+  ]
+})
+export class PerguntaModule { }
