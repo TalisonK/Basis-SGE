732c9468d1eb79532d3e1086a7adceeee134f5c6
talison <talisonkennedy1@gmail.com>
2021-02-01 10:11:37 -0300

    Implementação do sistema de listagem de inscrições(Back e Front)

---
+++ BackEnd/service/src/main/java/com/basis/sge/service/servico/dto/InscricaoListagemDTO.java
@@ -0,0 +1,18 @@
+package com.basis.sge.service.servico.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class InscricaoListagemDTO{
+
+    private String evento;
+    private String usuario;
+    private LocalDateTime dataInicio;
+    private LocalDateTime dataFim;
+    private String situacao;
+
+}
