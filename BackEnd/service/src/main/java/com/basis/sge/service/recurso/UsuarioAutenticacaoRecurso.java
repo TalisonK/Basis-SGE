99527990912930260fa9898fb9c234723d313454
Thayane Nunes <thayanenns@gmail.com>
2021-02-02 15:20:20 -0300

    Login backend

--- BackEnd/service/src/main/java/com/basis/sge/service/recurso/UsuarioAutenticacaoRecurso.java
+++ BackEnd/service/src/main/java/com/basis/sge/service/recurso/UsuarioAutenticacaoRecurso.java
@@ -1,13 +1,21 @@
 package com.basis.sge.service.recurso;
 
+import com.basis.sge.service.dominio.Usuario;
 import com.basis.sge.service.servico.UsuarioAutenticacaoServico;
 import com.basis.sge.service.servico.dto.UsuarioAutenticacaoDTO;
+import com.basis.sge.service.servico.dto.UsuarioDTO;
+import com.basis.sge.service.servico.exception.RegraNegocioException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 
 @RestController
@@ -17,9 +25,8 @@ public class UsuarioAutenticacaoRecurso {
 
     private final UsuarioAutenticacaoServico usuarioAutenticacaoServico;
 
-    @GetMapping()
-    public ResponseEntity<UsuarioAutenticacaoDTO> autenticacaoCpfEChave(@RequestBody UsuarioAutenticacaoDTO usuarioAutenticacaoDTO){
-
+    @PostMapping
+    public ResponseEntity<UsuarioDTO> autenticacaoCpfEChave(@Valid @RequestBody UsuarioAutenticacaoDTO usuarioAutenticacaoDTO) throws URISyntaxException, RegraNegocioException {
         return ResponseEntity.ok(usuarioAutenticacaoServico.autenticacaoCpfEChave(usuarioAutenticacaoDTO));
     }
 }
--- BackEnd/service/src/main/java/com/basis/sge/service/servico/UsuarioAutenticacaoServico.java
+++ BackEnd/service/src/main/java/com/basis/sge/service/servico/UsuarioAutenticacaoServico.java
@@ -3,8 +3,10 @@ package com.basis.sge.service.servico;
 import com.basis.sge.service.dominio.Usuario;
 import com.basis.sge.service.repositorio.UsuarioRepositorio;
 import com.basis.sge.service.servico.dto.UsuarioAutenticacaoDTO;
+import com.basis.sge.service.servico.dto.UsuarioDTO;
 import com.basis.sge.service.servico.exception.RegraNegocioException;
 import com.basis.sge.service.servico.mapper.UsuarioAutenticacaoMapper;
+import com.basis.sge.service.servico.mapper.UsuarioMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,11 +19,11 @@ public class UsuarioAutenticacaoServico {
 
     private final UsuarioRepositorio usuarioRepositorio;
 
-    private final UsuarioAutenticacaoMapper usuarioAutenticacaoMapper;
+    private final UsuarioMapper usuarioMapper;
 
-    public UsuarioAutenticacaoDTO autenticacaoCpfEChave(UsuarioAutenticacaoDTO usuarioAutenticacaoDTO) {
+    public UsuarioDTO autenticacaoCpfEChave(UsuarioAutenticacaoDTO usuarioAutenticacaoDTO) {
         Usuario usuario = usuarioRepositorio.findByCpfAndChave(usuarioAutenticacaoDTO.getCpf(), usuarioAutenticacaoDTO.getChave()).orElseThrow(() -> new RegraNegocioException("Autenticação negada"));
-        return usuarioAutenticacaoMapper.toDto(usuario);
+        return usuarioMapper.toDto(usuario);
     }
 
 }
