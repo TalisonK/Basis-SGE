022bcab002f1f82d2a54010bbe37e687065a3ed4
kayc-drummond-oda <kayc-drummond-oda>
2021-02-04 17:17:48 -0300

    adicionado Dialog para exclusao de Pergunta

--- frontend/src/app/modulos/pre-inscricao/pre-inscricao.module.ts
+++ frontend/src/app/modulos/pre-inscricao/pre-inscricao.module.ts
@@ -7,12 +7,9 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { RespostaBlocoComponent } from './componentes/resposta-bloco/resposta-bloco.component';
 import { InscricaoService } from './services/inscricao-service.service';
 import { ListagemInscricaoComponent } from './componentes/listagem-inscricao/listagem-inscricao.component';
-import { LoginTesteComponent } from './componentes/login-teste/login-teste.component';
-import { LonginFormComponent } from './componentes/login-teste/longin-form/longin-form.component';
-
 
 @NgModule({
-  declarations: [FormularioComponent, RespostaBlocoComponent, ListagemInscricaoComponent, LoginTesteComponent, LonginFormComponent],
+  declarations: [FormularioComponent, RespostaBlocoComponent, ListagemInscricaoComponent],
   imports: [
     CommonModule,
     PreInscricaoRoutingModule,
